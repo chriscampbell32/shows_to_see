653edcbcb283405b2e0432935016df9be48f7b1d
chriscampbell32 <chriscampbell32@yahoo.com>
2016-05-07 14:40:48 -0400

    move models into new folder

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 //connects to mongodb on modulus.io =====
- // mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');  
 var database = require('./config/database');
 mongoose.connect(database.url);
 
