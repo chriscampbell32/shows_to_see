5215db072af6959c404039453ee5e5c00cef3102
chriscampbell32 <chriscampbell32@yahoo.com>
2016-05-11 20:40:15 -0400

    put modulus.io connection in server.js so that it works again

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
-
 var mongoose = require('mongoose');
-
-var database = require('./config/database');
+// var database = require('./config/database');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -10,7 +8,7 @@ var methodOverride = require('method-override');
 var app = express();
 var port = process.env.PORT || 8080;
 //connects to mongodb on modulus.io =====
-mongoose.connect(database.url);
+mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
