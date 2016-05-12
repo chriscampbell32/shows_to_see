51b350023fdeeef420125aa432871c6bc00f30d6
chriscampbell32 <chriscampbell32@yahoo.com>
2016-05-11 20:18:13 -0400

    change port to process.env

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+var database = require('./config/database');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 //connects to mongodb on modulus.io =====
-var database = require('./config/database');
 mongoose.connect(database.url);
 
 app.use(express.static(__dirname + '/public'));
@@ -16,13 +17,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
-
-
 //routes =====
 
 require('./app/routes')(app);
 
 
-app.listen(8080);
-console.log("app listeninig on port 8080");
+app.listen(port);
+console.log("app listeninig on port " + port);
 
